19feb995cb9a065caeb4e319f5b6d284daf8d2ad
truongquangquoc2011 <truongquangquoc2011@gmail.com>
2025-02-27 20:16:50 +0700

    Final Commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-    $('.popup-with-zoom-anim').magnificPopup({
-        type: 'inline',  // Hiển thị nội dung của một phần tử HTML
-        removalDelay: 300,
-        mainClass: 'my-mfp-zoom-in',
-        preloader: false,
-        fixedContentPos: false
-    });
-});
+
